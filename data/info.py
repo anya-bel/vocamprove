f620c6ff8484b054c32384e4fd245b91ac231b78
Anna Mosolova <39431144+anya-bel@users.noreply.github.com>
2020-12-24 14:04:22 +0100

    dependency implemented

--- data/info.py
+++ data/info.py
@@ -9,13 +9,15 @@ import csv
 import spacy
 from spacy.kb import KnowledgeBase
 
+nlp = spacy.load("en_core_web_sm")
+
 # (mock input)
-text_index = 5
+text_index = 6
 sentence_index = 5
 word_index = 5
 
-word = 'project'
-POS_abb = 'v'
+word = 'difficulty'
+POS_abb = 'n'
 
 
 #(mock interactive input for debugging)
@@ -23,69 +25,85 @@ POS_abb = 'v'
 # sentence_index = input('sentence index:\n')
 # word_index = input('word index:\n')
 
-def locate():
-    path = './data/words.csv'
-
-    with open(path, 'r', newline='') as file:
-        reader = csv.reader(file, delimiter=',')
-        df = pd.DataFrame(reader)
-        
-        csv_row = df.loc[(df['text-index'] == text_index) & (df['word-index'] == word_index) & (df['sentence-index'] == sentence_index)]
-               
-        print(csv_row)
+def locate(text_index, sentence_index, word_index):
+    words_path = '../data/csv-files/words.csv'
+    words_df = pd.read_csv(words_path)
+    word_csv_row = words_df.loc[(words_df['text-index'] == text_index) & (words_df['word-index'] == word_index) & (words_df['sentence-index'] == sentence_index)]
+    sentences_path = '../data/csv-files/sentences.csv'
+    sentences_df = pd.read_csv(sentences_path)
+    sent_csv_row = sentences_df.loc[(sentences_df['text-index'] == text_index) & (sentences_df['sentence-index'] == sentence_index)]
+    return word_csv_row['word-index'].iloc[0], word_csv_row['word'].iloc[0].strip(), word_csv_row['POS'].iloc[0], sent_csv_row['sentence'].iloc[0]
 
 # WORD DEFINITION FROM WORDNET
-def find_definition():
-    columns=['Lemma', 'Definition', 'Example']
+def find_definition(word, POS):
+    columns=['Word', 'Definition', 'Example']
     definition = pd.DataFrame(columns=columns)
-                              
-    for synset in wn.synsets(word)[0:-1]:
-        
-        if synset.name()[-4] == POS_abb:
-        
-            # TO DO ► RegEx POS, definition and examples.
-            
-            #name = synset.name()[-4]
+    pos_wn = {'ADJ': 'a',
+              'SUBST': 'n',
+              'VERB': 'v',
+              'ADV': 'r'}
+    pos = pos_wn.get(POS)
+    if pos:
+        for synset in wn.synsets(word)[0:-1]:
+            if synset.name()[-4] == pos:
+                # TO DO ► RegEx POS, definition and examples.
+
+                define = synset.definition()
+                example = synset.examples()
+
+                syn = pd.DataFrame(np.array([[word, define, example]], dtype=object), columns=columns)
+
+                definition = definition.append(syn)
+    else:
+        for synset in wn.synsets(word)[0:-1]:
             define = synset.definition()
             example = synset.examples()
-
-            syn = pd.DataFrame(np.array([[name, define, example]], dtype=object), columns=columns)
-
+            syn = pd.DataFrame(np.array([[word, define, example]], dtype=object), columns=columns)
             definition = definition.append(syn)
 
+    if definition.empty:
+        syn = pd.DataFrame(np.array([[word, '', []]], dtype=object), columns=columns)
+        definition = definition.append(syn)
+        return definition
     return(definition)
 
 # DEPENDENCY
-def find_dependency():
-    
-    dependency = 'syntactic tag (ex. auxiliary)'
-    return(dependency)
+def find_dependency(idx, word, sentence):
+    doc = nlp(sentence)
+    for num, token in enumerate(doc):
+        if token.text == word:
+            if idx-1 <= num <= idx+1:
+                dependency = token.dep_
+                break
+    return dependency
 
+
+"""
 # POS
 def find_POS():
-    nlp = spacy.load("en_core_web_sm") 
     doc = nlp(sentence)
     POS = word, word.lemma_
     return(POS)
+"""
     
 # PRONUNCIATION
-def find_pronunciation():
+def find_pronunciation(word):
     tts = gTTS(word)
     tts.save(word+'.mp3')
+    return word+'.mp3'
 
 # OUTPUT
-def word_info():
-    print('Definition:')
-    print(definition)
-    print('Dependency:')
-    print(dependency)
-    print('POS:')
-    print(POS)
+def word_info(definition, dependency, POS, audiopath):
+    print(f'Definition: {definition}')
+    print(f'Dependency: {dependency}')
+    print(f'POS: {POS}')
+    print(f'Path to mp3: {audiopath}')
 
 if __name__ == "__main__":
-    locate()
-    definition = find_definition()
-    dependency = find_dependency()
-    POS = find_POS()
-    find_pronunciation()
-    word_info()
+    idx, word, POS, sentence = locate(text_index, sentence_index, word_index)
+    print(sentence)
+    definition = find_definition(word, POS)
+    dependency = find_dependency(idx-1, word, sentence)
+    #POS = find_POS(word)
+    audiopath = find_pronunciation(word)
+    word_info(definition, dependency, POS, audiopath)
