4a95c94a9d9bf678dec9a1b2718d2c875d9310e4
Elisa Lubrini <61165479+e-lubrini@users.noreply.github.com>
2021-03-01 19:20:36 +0100

    Defined functions to calculate the human evaluation score

--- evaluation/ue_evaluation/quality score.py
+++ evaluation/ue_evaluation/quality score.py
@@ -1,57 +1,80 @@
 from math import sqrt
-from numpy import mean, isnan
+import numpy as np
 import pandas as pd
 
-def human_evaluation(results):
-    n_tot=len(set(df['dimension']))
-    D_sum = 0
-    c=0
-    for Dim in set(dimension):
-        df_Dim = df.loc[df["dimension"]==Dim]
-        factors = (set(df_Dim['factor']))
-        for n in factors:
-            Dim_j = 0
-            factor_o = 0
-            factor_u = 0
-            df_f = df_Dim.loc[df_Dim['factor'] == n]
-            for row in df_f.iterrows():
-                r = dict(zip(header,list(row[1])))
-                factor_o += (r['q_weight'])*(r['avg'])
-                factor_u += r['q_weight']
-            p = r['f_weight']
-            factor_n = (factor_o/factor_u)
-            Dim_j += p*factor_n
-        D_sum += (1-(Dim_j/100))**2
-    D = sqrt(D_sum)
-    # The real system quality is then computed
-    q = (1-(D/sqrt(n_tot)))*100
-    return(q)
+file = "survey_results.csv"
 
-path = "/content/drive/MyDrive/Uni_Projects/Vocamprove/eva.csv"
 
-df = pd.read_csv(path)
-df = df.T
-df = df.reset_index(drop=True)
-df = df.drop(0)
-col_1 = list((df[1]))
-print(col_1)
-col_int= [int(s) for s in col_1 if ((isinstance(s, int) or isinstance(s, float))and str(s) != 'nan')]
-print(col_int)
-df = df.loc[col_int]
-df = df.reset_index(drop=False)
-df = df.astype(str).astype(int)
+def score_table(path):
+    df = pd.read_csv(path)
+    df = df.T
+    df = df.reset_index(drop=True)
+    df = df.drop(0)
 
-q_weight = [0.5,0.5, 0.2,0.2,0.2,0.2,0.2, 0.25,0.25,0.25,0.25, 1, 0.5,0.5, 1,1,1,1, 0.25,0.25,0.25,0.25, 1]
-factor = [1,1,2,2,2,2,2,3,3,3,3,4,5,5,6,7,8,9,10,10,10,10,11]
-f_weight = [1 for i in range(len(q_weight))]
-dimension = [1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3]
+    df = df.apply(lambda x: pd.to_numeric(x, errors='coerce')).dropna()
 
-df['avg']= df.mean(numeric_only=True, axis=1)*20
-df['q_weight']=q_weight
-df['factor']=factor
-df['f_weight']=f_weight
-df['dimension']=dimension
-header = list(df)
+    df = df.reset_index(drop=True)
+
+    q_weight = [0.5, 0.5, 0.2, 0.2, 0.2, 0.2, 0.2, 0.25, 0.25, 0.25, 0.25, 1, 0.5, 0.5, 1, 1, 1, 1, 0.25, 0.25, 0.25,
+                0.25, 1]
+    factor = [1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 5, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+    f_weight = [1 for i in range(len(q_weight))]
+    dimension = [1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3]
+
+    df['avg'] = df.mean(numeric_only=True, axis=1) * 20
+    df['q_weight'] = q_weight
+    df['factor'] = factor
+    df['f_weight'] = f_weight
+    df['dimension'] = dimension
+    header = list(df)
+    print(df)
+    return df
+
+
+def get_factor_score(df):
+    factor_score = []
+    prod = df.avg * df.q_weight
+    for i in set(df.factor):
+        index = df.factor == i
+        divid = np.sum(prod[index])
+        divis = np.sum(df.q_weight[index])
+        factor_score.append(divid / divis)
+    return np.array(factor_score)
+
+def get_factor_weight(df):
+    current_factor = int(df.factor[0])
+    indexes = [0]
+    for i in range(len(df.factor)):
+        if df.factor[i] != current_factor:
+            indexes.append(i)
+        current_factor=df.factor[i]
+    return np.array(df.f_weight[indexes])
+
+def get_dim_score(df, factor_scores):
+    dim_score = []
+    prod = get_factor_weight(df) * factor_scores
+
+    current_dim =  df.dimension[0]
+    indexes = [0]
+    for i in range(len(set(df.dimension))):
+        if df.dimension[i] != current_dim:
+            indexes.append(i)
+        current_dim=df.dimension[i]
+
+    dim_score.append(prod[indexes])
+    return np.array(dim_score)
+
+
+def get_human_evaluation_score(df):
+    factor_scores = get_factor_score(df)
+    dim_scores = get_dim_score(df, factor_scores)
+    D = np.sqrt(np.sum(1 - (dim_scores / 100)))
+    n = len(set(df.dimension))
+    q = (1-(D/np.sqrt(n)))
+    return q
+
 
 if __name__ == "__main__":
-	human_evaluation(df)
+    df = score_table(file)
+    print(get_human_evaluation_score(df))
+
